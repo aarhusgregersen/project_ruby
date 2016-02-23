a9425e578e6e686d1b08aecfaed22024f89ad2e5
Martin Gregersen JEF <mag@jef.dk>
2016-02-23 15:33:35 +0100

    Starts pig latin

---
+++ 04_pig_latin/pig_latinb.rb
@@ -0,0 +1,9 @@
+consonants = 'b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,z,y,x'
+consonants = consonants.split(',')
+
+def translate(input)
+	if input[0] = consonant
+	  input[1...] + consonant + 'ay'
+	else  
+		input + 'ay'
+end
